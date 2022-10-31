79aa36028e615866d4f1ce41032be5fe74e3dcc4
Henry Santana <Oneyver41@gmail.com>
2022-10-30 20:17:18 -0400

    cambio en la valdiacion de sessiones

--- plantillas/cabeceraEmpleado.php
+++ plantillas/cabeceraEmpleado.php
@@ -20,5 +20,22 @@ if(empty($_SESSION)){
 </head>
 <body>
     <header>
-        
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
+  <div class="container-fluid">
+    <h1 class="navbar-brand">Inventario</h1>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="empleado.php">Home <i class="fa-solid fa-house"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="../modelo/cerrarSesion.php">Cerrar sesion<i class="fa-solid fa-door-open"></i></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
     </header>
