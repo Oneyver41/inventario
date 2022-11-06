4fa6585bfb376805d5458262e82e511918c6fb09
Henry Santana <Oneyver41@gmail.com>
2022-11-06 11:55:01 -0400

    modificaondos footer

--- plantillas/footerCliente.php
+++ plantillas/footerCliente.php
@@ -1,10 +1,16 @@
 <footer>
-
-
-
-
-
-
+<div class="bg-dark text-secondary px-4 py-5 text-center mt-4">
+    <div class="py-5">
+      <h1 class="display-5 fw-bold text-white">Nuestro proposito</h1>
+      <div class="col-lg-6 mx-auto">
+        <p class="fs-5 mb-4">Queremos ayudar a las empresas mayoristas o minimalistas a que puedan mejorar la efciencia de su manejo en el inventario y poder tener un control optimo y seguro para que manejen su mercancia y puedan atenter a su clientela y controlar el registro de cada producto que y inventariado de la mercancia, todo puede ser mas sencillo de controlar.</p>
+        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+          <a href="login.php"><button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Empecemos!</button></a>
+          <a href="desarollador.php"><button type="button" class="btn btn-outline-light btn-lg px-4">Creador del sitio</button></a>
+        </div>
+      </div>
+    </div>
+</div>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
         integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
       </script>
